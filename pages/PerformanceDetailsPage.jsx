6247c6bf25415bbddba1effd4ab7a7e22ed95322
Fran Martinez <pmg.webdev@gmail.com>
2023-12-06 18:32:53 +0100

    displays performances list and details

--- pages/PerformanceDetailsPage.jsx
+++ pages/PerformanceDetailsPage.jsx
@@ -1,7 +1,52 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+useState;
+
+const urlAPI = import.meta.env.VITE_API_URL;
+
 function PerformanceDetailsPage() {
-    return(
-        <h1>PerformanceDetailsPage</h1>
-    )
+  const [loading, setLoading] = useState(true);
+  const [performance, setPerformance] = useState({});
+  const { performanceId } = useParams();
+
+  function refreshPage() {
+    window.location.reload(false);
+  }
+
+  function getPerformance() {
+    setLoading(true);
+    axios
+      .get(`${urlAPI}/api/performances/${performanceId}`)
+      .then((response) => {
+        setPerformance(response.data);
+      })
+      .catch((error) => {
+        console.log("Error: " + error);
+      });
+  }
+
+  useEffect(() => {
+    getPerformance();
+  }, []);
+
+  return (
+    <div className="PerformanceDetailsPage">
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <div>
+          <h2>{performance.title} </h2>
+          <p>{performance.description}</p>
+          <p>{performance.requirements} </p>
+          <span>{performance.fee} </span>
+          {performance.typeOfPerformance.map((type) => {
+            return <span>type</span>;
+          })}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default PerformanceDetailsPage;
--- pages/PerformancesPage.jsx
+++ pages/PerformancesPage.jsx
@@ -1,7 +1,55 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+useState;
+
+const urlAPI = import.meta.env.VITE_API_URL;
+
 function PerformancesPage() {
-    return(
-        <h1>PerformancesPage</h1>
-    )
+  const [loading, setLoading] = useState(true);
+  const [performances, setPerformances] = useState([]);
+
+  function refreshPage() {
+    window.location.reload(false);
+  }
+  function getAllPerformances() {
+    setLoading(true);
+    axios
+      .get(`${urlAPI}/api/performances`)
+      .then((response) => {
+        setEvents(response.data);
+      })
+      .catch((error) => {
+        console.log("Error: " + error);
+      });
+  }
+
+  useEffect(() => {
+    getAllPerformances();
+  }, []);
+
+  return (
+    <div className="PerformancesPage">
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <div className="performances-list">
+          {performances.map((performance) => {
+            return (
+              <div className="performance-card" key={performance.id}>
+                <h2>{performance.title} </h2>
+                <p>{performance.description}</p>
+                <p>{performance.requirements} </p>
+                <span>{performance.fee} </span>
+                {performance.typeOfPerformance.map((type) => {
+                  return <span>type</span>;
+                })}
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default PerformancesPage;
