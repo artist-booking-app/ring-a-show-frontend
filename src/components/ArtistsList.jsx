7045fce38564bbba7f2e648fdbe8e88cda22c483
Fran Martinez <pmg.webdev@gmail.com>
2023-12-07 20:22:54 +0100

    Creates List component for Performances and Artists

---
+++ src/components/ArtistsList.jsx
@@ -0,0 +1,66 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+function ArtistsList(props) {
+  const API_URL = import.meta.env.VITE_API_URL;
+
+  const [artists, setArtists] = useState([]);
+
+  const getApiData = () => {
+    axios
+      .get(`${API_URL}/api/artists`)
+      .then((response) => {
+        setArtists(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    getApiData();
+  }, []);
+
+  return (
+    <>
+      <Link to="/favourites">Favourites</Link>
+
+      {artists === null ? (
+        <p>Loading...</p>
+      ) : (
+        artists.map((elm) => {
+          return (
+            <>
+              <ul>
+                {artists.map((artist) => (
+                  <li className="artist-info">
+                    <Link to={`/artists/${artist._id}`}>
+                    <h3>{artist.artistName}</h3>
+                    </Link>
+                    {/* <img src={artist.imagePath} alt="artist picture" /> */}
+                    <p>Type: {artist.typeOfPerformance}</p>
+                    <p>Genre: {artist.genre}</p>
+                    <p>Available in: {artist.cityLocation}</p>
+                    {/* <p>{artist.about.biography}</p>
+<a>{artist.about.showreel}</a> */}
+                  </li>
+                ))}
+              </ul>
+
+              <button
+                onClick={() => {
+                  props.callback(elm._id);
+                }}
+              >
+                Add to Favourites
+              </button>
+            </>
+          );
+        })
+      )}
+    </>
+  );
+}
+
+export default ArtistsList;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,16 @@
+import PerformancesList from "../components/PerformancesList";
+import ArtistsList from "../components/ArtistsList";
+
+
 function HomePage() {
     return(
-        <h1>HomePage</h1>
+        <>
+            <h1>HomePage</h1>
+        
+        {/* <PerformancesList /> */}
+        <ArtistsList />
+        </>
+        
     )
 }
 
