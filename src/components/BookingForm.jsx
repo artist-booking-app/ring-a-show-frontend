86beb8f135878f7443181119afa2416093a2d065
Fran Martinez <pmg.webdev@gmail.com>
2023-12-11 11:51:05 +0100

    connects booking ref with user in db

--- src/components/BookingForm.jsx
+++ src/components/BookingForm.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../context/auth.context";
 
 function BookingForm({ performanceId, artistId, API_URL }) {
@@ -10,6 +10,9 @@ function BookingForm({ performanceId, artistId, API_URL }) {
     const [indoor, setIndoor] = useState(true)
     const [date, setDate] = useState("")
     const {user} = useContext(AuthContext)
+
+    const [bookingRefArr, setBookingRefArr] = useState([])
+
 
     const bookNowHandler = (e) => {
         e.preventDefault()
@@ -26,94 +29,105 @@ function BookingForm({ performanceId, artistId, API_URL }) {
             }
         }
 
-        // console.log(requestBody)
+        axios
+        .post(API_URL + "/api/bookings", requestBody)
+        .then((response) => {
+          console.log(response + "Booking was successful");
+          console.log(response.data._id);
 
-        axios.post(API_URL + "/api/bookings", requestBody)
+          const bookingId= response.data._id
+
+  
+          axios
+            .put(API_URL + `/api/users/${user._id}`, {$push: {bookingReference: bookingId}})
             .then((response) => {
-                console.log(response + "Booking was successful")
-                console.log(response.data)
-            })
-            .catch((error) => {
-                console.log(error)
-            })
+              console.log(response.data , "user details");
+          
+            });
+        })
+  
+        .catch((error) => {
+          console.log(error);
+        });
+    };
         
-        
+    return (
+      <>
+          <h1>Booking page</h1>
+
+          <form onSubmit={bookNowHandler}>
+
+              <label>
+                  Address:
+                  <input
+                      type="text"
+                      name="address"
+                      required={true}
+                      value={address}
+                      onChange={(e) => setAddress(e.target.value)}
+                  />
+              </label>
+
+              <label>
+                  Date:
+                  <input
+                      type="datetime-local"
+                      name="datetime"
+                      required={true}
+                      value={date}
+                      onChange={(e) => setDate(e.target.value)}
+                  />
+              </label>                
+
+              <label>
+                  Type of Location:
+                  <select
+                      type="option"
+                      name="type of location"
+                      required={true}
+                      value={typeOfLocation}
+                      onChange={(e) => setTypeOfLocation(e.target.value)}
+                  >
+                      <option disabled select value="">
+                          {" "}
+                          Select
+                      </option>
+                      <option value="Private">Private</option>
+                      <option value="Commercial">Commercial</option>
+
+                  </select>
+              </label>
+
+              <label>Indoor?</label>
+              <label>
+                  <input
+                      type="radio"
+                      name="indoor"
+                      value="true"
+                      checked={indoor === true}
+                      onChange={() => setIndoor(true)}
+                  />
+                  Yes
+              </label>
+              <label>
+                  <input
+                      type="radio"
+                      name="outdoor"
+                      value="false"
+                      checked={indoor === false}
+                      onChange={() => setIndoor(false)}
+                  />
+                  No
+              </label>
+
+              <button>Book Now</button>
+          </form>
+      </>
+
+  )  
     }
 
-    return (
-        <>
-            <h1>Booking page</h1>
+    
 
-            <form onSubmit={bookNowHandler}>
-
-                <label>
-                    Address:
-                    <input
-                        type="text"
-                        name="address"
-                        required={true}
-                        value={address}
-                        onChange={(e) => setAddress(e.target.value)}
-                    />
-                </label>
-
-                <label>
-                    Date:
-                    <input
-                        type="datetime-local"
-                        name="datetime"
-                        required={true}
-                        value={date}
-                        onChange={(e) => setDate(e.target.value)}
-                    />
-                </label>                
-
-                <label>
-                    Type of Location:
-                    <select
-                        type="option"
-                        name="type of location"
-                        required={true}
-                        value={typeOfLocation}
-                        onChange={(e) => setTypeOfLocation(e.target.value)}
-                    >
-                        <option disabled select value="">
-                            {" "}
-                            Select
-                        </option>
-                        <option value="Private">Private</option>
-                        <option value="Commercial">Commercial</option>
-
-                    </select>
-                </label>
-
-                <label>Indoor?</label>
-                <label>
-                    <input
-                        type="radio"
-                        name="indoor"
-                        value="true"
-                        checked={indoor === true}
-                        onChange={() => setIndoor(true)}
-                    />
-                    Yes
-                </label>
-                <label>
-                    <input
-                        type="radio"
-                        name="outdoor"
-                        value="false"
-                        checked={indoor === false}
-                        onChange={() => setIndoor(false)}
-                    />
-                    No
-                </label>
-
-                <button>Book Now</button>
-            </form>
-        </>
-
-    )
-}
 
 export default BookingForm
