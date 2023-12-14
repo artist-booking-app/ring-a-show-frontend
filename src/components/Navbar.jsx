cf0d2151d26f8c34d7553615a53c07346fbf362a
Jaime Ilustre <jaimeilustre@yahoo.com>
2023-12-14 20:40:11 +0100

    added hamburger menu and added responsiveness to other pages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,25 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../context/auth.context";
 import "../components/Navbar.css";
 import logo from "../assets/logo-black.png";
+import hamburgerIcon from "../assets/hamburger.svg"
+
 function Navbar() {
+
   const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
 
   const userId = user?._id;
 
+  const [hamburgerOpen, setHamburgerOpen] = useState(false);
+  const toggleHamburger = () => {
+      setHamburgerOpen(!hamburgerOpen)
+  }
+
   return (
+    <>
     <nav className="Navbar">
+
       <div className="logo">
       <Link to="/">
         <img src={logo} alt="Logo" />
@@ -22,7 +32,6 @@ function Navbar() {
       {/*    UPDATE     */}
       {isLoggedIn && (
         <>
-
           <button onClick={logOutUser}>Logout</button>
         </>
       )}
@@ -53,8 +62,54 @@ function Navbar() {
         </>
       )}
       </div>
-      
+      <img className="hamburger-logo" src={hamburgerIcon} onClick={toggleHamburger} />
     </nav>
+
+  {hamburgerOpen &&
+    <nav className="hamburger-nav">
+         {isLoggedIn && (
+       <ul>
+       <li>
+       <Link to={`/artists/create`}>
+             Create Artist
+             </Link>
+       </li>
+       <li>
+       <Link to={`/performances/create`}>
+             Create Performance
+             </Link>
+       </li>
+           <li>
+             <Link to={`/users/${userId}`}>
+               My Profile{" "}
+             </Link>
+           </li>
+           <li>
+             <button onClick={logOutUser}>Logout</button>
+           </li>
+     </ul>
+      )}
+
+      {!isLoggedIn && (
+        <ul>
+          <li>
+          <Link to="/signup">
+            {" "}
+           Sign Up{" "}
+          </Link>
+          </li>
+          <li>
+          <Link to="/login">
+            {" "}
+            Login{" "}
+          </Link>
+          </li>
+        </ul>
+      )}
+
+    </nav>
+    }
+    </>
   );
 }
 
